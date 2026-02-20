fa7c59dac5201f0d12e1fffb6c484ca7c64e6fa5
madSob <gamingbrn202@gmail.com>
2026-02-20 15:09:22 +0700

    mengganti baseURL

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,3 +1,4 @@
 class Config {
-  static const String baseUrl = "https://fitlife-web-platform.vercel.app/api";
+  // static const String baseUrl = "https://fitlife-web-platform.vercel.app/api";
+  static const String baseUrl = "http://192.168.1.6:8000/api";
 }
